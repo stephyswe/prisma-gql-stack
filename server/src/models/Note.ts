2e70393f9dc6d2987a4608078d46c2297dcc004f
Stephanie <stephaniiehallberg@gmail.com>
2022-10-12 18:40:34 +0200

    backend: graphql schema

---
+++ server/src/models/Note.ts
@@ -0,0 +1,17 @@
+// server/src/models/Note.ts
+
+import { builder } from "../builder";
+
+builder.prismaObject("Note", {
+  findUnique: (note) => ({ id: note.id }),
+  fields: (t) => ({
+    id: t.exposeID("id"),
+    message: t.exposeString("message"),
+    createdAt: t.expose("createdAt", {
+      type: "Date",
+    }),
+    updatedAt: t.expose("updatedAt", {
+      type: "Date",
+    }),
+  }),
+});
--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,4 +1,8 @@
 // server/src/schema.ts
-
 import { builder } from "./builder";
+
+// 👇 Added the models here
+import "./models/User";
+import "./models/Note";
+
 export const schema = builder.toSchema({});
